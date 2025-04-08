a37687bf6dca0036c6d136c061df10c812564506
Sheeeennn <127721330+Sheeeenn@users.noreply.github.com>
2025-04-08 18:38:58 +0800

    [New] Firebase

---
+++ patchup/config/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0lJojPR5Zw2GG6ZOhs-NNqrNJQ2SE-EE",
+  authDomain: "patch-app-482b7.firebaseapp.com",
+  projectId: "patch-app-482b7",
+  storageBucket: "patch-app-482b7.firebasestorage.app",
+  messagingSenderId: "195104096980",
+  appId: "1:195104096980:web:eda07c2528473b0366b141"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
